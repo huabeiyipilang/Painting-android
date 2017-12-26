7d39524046d197348f9c532a7624a0554852c9c7
likai <likai12@baidu.com>
2017-12-26 16:28:15 +0800

    project init

---
+++ app/src/main/java/com/penghaonan/painting/App.java
@@ -0,0 +1,15 @@
+package com.penghaonan.painting;
+
+import android.app.Application;
+
+import com.penghaonan.appframework.AppDelegate;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppDelegate.init(this);
+        AppDelegate.setDebug(BuildConfig.DEBUG);
+    }
+}
